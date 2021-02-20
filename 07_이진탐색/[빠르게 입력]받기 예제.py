671a25becdbcc61482549187832f0c343300397d
KimKwangHui <hgi7201@naver.com>
2021-02-20 21:25:20 +0900

    2021-02-20

--- 07_이진탐색/[빠르게 입력]받기 예제.py
+++ 07_이진탐색/[빠르게 입력]받기 예제.py
@@ -23,15 +23,14 @@
 
 [ 입력 ]
 Hello, Coding Test!
-"""
-# 빠르게 입력받기
-# 시간초과 피할 수 있음
 
+"""
+# 빠르게 입력받기 --> 시간초과 피할 수 있음
 # 7-4.py 한 줄 입력받아 출력하는 소스 코드 .
 import sys
 
 # 하나의 문자열 데이터 입력바더기
-input_data = sys.stdin.readline().rstrip() # 줄 바꿈을 제거 하기 위해 rstrip() 함수 사
+input_data = sys.stdin.readline().rstrip() # 줄 바꿈을 제거 하기 위해 rstrip() 함수 사용
 
 # 입력받은 문자열 그대로 출력
 print(input_data)
