2598c47756b7cdd23f89f1203af405309c9e3815
KimKwangHui <hgi7201@naver.com>
2021-02-20 19:04:21 +0900

    2021-02-20

--- 07_이진탐색/반복문으로 구현한 [이진 탐색] 예제.py
+++ 07_이진탐색/반복문으로 구현한 [이진 탐색] 예제.py
@@ -1,7 +1,6 @@
-"""
-< 이진 탐색: 반으로 쪼개면서 탐색하기 > p, 188 ~190
-- 이진 탐색(Binary Search)은 [ ★ 배열 내부의 데이터가 정렬 ★ ]되어 있어야만 사용 가능.
 
+"""
+< 
 < 이진 탐색의 특징 >
 - 데이터가 무작위일 때는 사용 x
   [ 이미 정렬되어 있다면 매우 빠르게 ] 데이터를 찾을 수 있다는 특징.
@@ -33,28 +32,32 @@
 
 1 3 5 6 9 11 13 15 17 19               원소가 존재하지 않습니다.
 
+★ [ 탐색 범위가 ] 2,000만을 넘어가면 [ 이진 탐색으로 ] 문제에 접근해 보길 권함
+- 처리해야 할 [ 데이터의 개수 ]나 [ 값이 ] 1,000만 단위 이상으로 넘어가면 [ 이진 탐색 ]과 같이
+ <  O(logN)의 속도를 내야 하는 알고리즘을 떠올려야 > 문제를 풀 수 있는 경우가 많다는 점을 기억.
 """
-# 7-2.py 재귀 함수로 구현한 [ 이진 탐색 ] 소스코드
-# 이진 탐색 소스코드 구현 (재귀 함수)
+# 7-3.py 반복문으로 구현한 [ 이진 탐색 ] 소스코드
+
+# 이진 탐색 소스코드 구현 (반복문)
 def binary_search(array, target, start, end):
-    if start > end:
-        return None # None은 값의 부재를 나타내는 데 자주 사용
-    mid = (start + end) // 2 # 중간점을 구함. -- 소수점 이하를 버림.
-    # 찾은 경우 [ 중간점 인덱스 반환 ]
-    if array[mid] == target:
-        return mid
-    # [ 중간점의 값보다 ] 찾고자 하는 값이 작은 경우 '왼쪽' 확인
-    # [ 중간점 이 후의 값은 ] 필요 없으니, 끝점을 [ 중간점 -1 로 옮긴다 ].
-    elif array[mid] > target:
-        return binary_search(array, target, start, mid - 1) 
-    # [ 중간점의 값보다 ] 찾고자 하는 값이 큰 경우 '오른쪽' 확인
-    else:
-        return binary_search(array, target, mid + 1, end)
+    while start <= end:
+        mid = (start + end) // 2
+        # 찾은 경우 중간점 인덱스 반환
+        if array[mid] == target:
+            return mid
+        
+        # [ 중간점의 값보다 ] 찾고자 하는 값이 작은 경우 '왼쪽' 확인
+        elif array[mid] > target:
+            end = mid - 1 #[ 중간점 이 후의 값은 ] 필요 없으니, '끝점'을 [ 중간점 -1 로 옮긴다 ].
+            
+        # 중간점의 값보다 찾고자 하는 값이 큰 경우 오른쪽 확인
+        else:
+            start = mid + 1
+    return None  # 아무것도 찾지 않는다면 None을 반환.
 
 # n(원소의 개수)과 target(찾고자 하는 값)을 입력 받기
 n, target = list(map(int, input().split()))
-
-# [ 전체 원소 입력 ] 받기
+# 전체 원소 입력 받기
 array = list(map(int, input().split()))
 
 # 이진 탐색 수행 결과 출력
@@ -63,3 +66,5 @@ if result == None:
     print("원소가 존재하지 않습니다.")
 else:
     print(result + 1)
+
+
