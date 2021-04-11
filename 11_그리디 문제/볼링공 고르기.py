3dc5adf6372e5a92a69b6a8844071f7d141a75f6
KimKwangHui <hgi7201@naver.com>
2021-04-11 23:29:41 +0900

    2021-04-10

--- 11_그리디 문제/볼링공 고르기.py
+++ 11_그리디 문제/볼링공 고르기.py
@@ -35,20 +35,20 @@
 1 5 4 3 2 4 5 2                           25
 """
 
-n, m = map(int, input().split())
+n, m = map(int, input().split()) # 볼링공의 개수, 볼링공의 [ 최대 무게 ] 입력.
 data = list(map(int, input().split()))
 
 # 1부터 10까지의 무게를 담을 수 있는 리스트
 array = [0] * 11
 
 for x in data:
-    # 각 무게에 해당하는 볼링공의 개수 카운트
+    # 각 무게에 해당하는 [ 볼링공의 개수 ] 카운트
     array[x] += 1
 
 result = 0
-# 1부터 m까지의 각 무게에 대하여 처리
+# 1부터 m(최대무게) 까지의 [ 각 무게에 대하여 처리 ]
 for i in range(1, m + 1):
-    n -= array[i] # 무게가 i인 볼링공의 개수(A가 선택할 수 있는 개수) 제외
+    n -= array[i] # [ 무게가 i인 ] 볼링공의 개수(A가 선택할 수 있는 개수) 제외
     result += array[i] * n # B가 선택하는 경우의 수와 곱해주기
 
 print(result)
